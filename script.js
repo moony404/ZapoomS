35b8eb5428065a2496bda4d6a69f9f1c0966a6af
moony404 <theo.marliac63@gmail.com>
2024-12-02 22:36:34 +0100

    test

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ document.getElementById('buttontheme').onclick = () => {
   document.documentElement.setAttribute("data-theme", newTheme);
   localStorage.setItem( "theme" , newTheme );
 };
+
+// bubble part
+// Sélectionner le bouton
+const button = document.querySelector('.button');
+
+// Fonction pour démarrer l'animation
+function startAnimation() {
+  button.classList.add('animate');
+
+  // Retirer la classe après la durée de l'animation (2.5 secondes)
+  setTimeout(() => {
+    button.classList.remove('animate');
+  }, 2500);
+}
+
+// Ajouter un écouteur d'événement pour le survol
+button.addEventListener('mouseenter', startAnimation);
