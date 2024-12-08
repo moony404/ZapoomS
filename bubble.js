7e5d6aed60f333bd5cce044ebb2edbbdf7a1fd0c
moony404 <theo.marliac63@gmail.com>
2024-12-08 22:06:38 +0100

    clean bubble.js line 231 (nothing much)

--- bubble.js
+++ bubble.js
@@ -228,7 +228,7 @@ function updateBlobColor() {
   console.log("testststetsetsetsetsetsetsetsetsesetsetsetsetsetsetsetsetsettse")
   if(blob && blob.render) {
     blob.color = blob.color; // Force le redessin avec la nouvelle couleur
-    blob.render(); // Redessine le blob } } document.getElementById('buttontheme').addEventListener('click', updateBlobColor); // Initialisation du blob comme avant blob = new Blob
+    blob.render(); // Redessine le blob 
   }
 }
 
