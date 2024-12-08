f6fd03f9842b3f29fec162d66c4da2a85da7bed7
moony404 <theo.marliac63@gmail.com>
2024-12-08 15:18:33 +0100

    update

--- smartphones_resize.js
+++ smartphones_resize.js
@@ -1,16 +1,7 @@
-function resizeCanvas() {
-  const canvas = document.getElementById('myCanvas');
-  const isMobile = window.innerWidth <= 768;
-
-  if (isMobile) {
-    canvas.width = window.innerWidth * 0.9; // Ajuste à 90% de la largeur de l'écran
-    canvas.height = canvas.width * 0.75; // Exemple : Ratio 4:3
-  } else {
-    canvas.width = 800; // Taille par défaut
-    canvas.height = 600;
-  }
-}
-
-// Redimensionner au chargement et à chaque modification de la fenêtre
-window.addEventListener('load', resizeCanvas);
-window.addEventListener('resize', resizeCanvas);
+let resize = function () {
+  // Assurez-vous que le canvas occupe la totalité de l'écran sans déborder
+  canvas.width = window.innerWidth; // Largeur = taille de l'écran
+  canvas.height = window.innerHeight; // Hauteur = taille de l'écran
+};
+window.addEventListener("resize", resize);
+resize();
