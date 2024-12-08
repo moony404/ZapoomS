994197b2d796223a83730ab1f932d48ea2571c6e
moony404 <theo.marliac63@gmail.com>
2024-12-08 19:50:24 +0100

    update

--- bubble.js
+++ bubble.js
@@ -269,7 +269,7 @@ init = function() {
       angle = Math.atan2(vector.y, vector.x);
       hover = true;
       canvas.style.cursor = "pointer"; // add to the pointer proprety to the cursor so it can be like a link
-      blob.color = '#efc621';
+      // blob.color = '#efc621'; // yellow (sort of)
     } else if(dist > blob.radius && hover === true){ // if to know if the cursor ISN'T on the bubble
       let vector = { x: e.clientX - pos.x, y: e.clientY - pos.y };
       angle = Math.atan2(vector.y, vector.x);
@@ -311,7 +311,7 @@ init = function() {
   blob.render();
 }
 
-const theme = document.documentElement.getAttribute('data-theme');
+const theme = document.documentElement.getAttribute('theme');
 console.log(theme); // Affiche "light" ou "dark"
 
 if (document.documentElement.getAttribute('theme') === 'dark') {
@@ -320,7 +320,7 @@ if (document.documentElement.getAttribute('theme') === 'dark') {
 } else {
   console.log('Le thème est clair');
   console.log(document.documentElement.getAttribute('theme'))
-  blob.color = "#d00000"; // Change la couleur du blob en rouge
+  // blob.color = "#d00000"; // Change la couleur du blob en rouge
 }
 
 // blob.color = "#229200"; // Change la couleur du blob en rouge
