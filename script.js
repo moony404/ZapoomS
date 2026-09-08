3a1c258a0e876cd49905bfa139412389dca85fb5
theo <theo.marliac63@gmail.com>
2026-09-08 22:37:16 +0200

    Agent Host changes for agents/cursor-hand-grab-central-window

--- script.js
+++ script.js
@@ -109,13 +109,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 // Make the DIV element draggable:
-dragElement(document.getElementById("mydiv"));
+const draggableWindow = document.getElementById("mydiv");
+if (draggableWindow) {
+  dragElement(draggableWindow);
+}
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  const dragHandle = document.getElementById(elmnt.id + "header");
   if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    dragHandle.onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
@@ -127,6 +131,7 @@ function dragElement(elmnt) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
+    dragHandle?.classList.add("dragging");
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
@@ -149,5 +154,6 @@ function dragElement(elmnt) {
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
+    dragHandle?.classList.remove("dragging");
   }
 }
